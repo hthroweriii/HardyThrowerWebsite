72a9050d5511f417fca5af52c2b67967a9055956
Hardy Thrower <hardyathrower@gmail.com>
2020-05-03 18:33:52 -0400

    update install

--- InstallApp.ps1
+++ InstallApp.ps1
@@ -1,7 +1,7 @@
 #TODO find out better way to call dotnet instead of using a hard coded location.
 #Unfortuantely there are multiple versions of  dotnet installed and the deployment always chooses the wrong one :(
 C:\Users\Administrator\AppData\Local\Microsoft\dotnet\dotnet.exe restore
-C:\Users\Administrator\AppData\Local\Microsoft\dotnet\dotnet.exe dotnet publish --configuration release -o c:\HardyWebsite\publish 
+C:\Users\Administrator\AppData\Local\Microsoft\dotnet\dotnet.exe publish --configuration release -o c:\HardyWebsite\publish 
 
 C:\Windows\System32\inetsrv\appcmd.exe stop site "HardyWebsite"
 C:\Windows\System32\inetsrv\appcmd.exe start site "HardyWebsite" 
