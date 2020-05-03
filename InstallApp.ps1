3e3c261c0df05471414edcde308c321868ec5cc1
Hardy Thrower <hardyathrower@gmail.com>
2020-05-03 10:32:28 -0400

    Fix InstallApp

--- InstallApp.ps1
+++ InstallApp.ps1
@@ -1,6 +1,8 @@
 dotnet restore
 
-dotnet publish --configuration release -o c:\HardyWebsite\publish --runtime active
+dotnet publish --configuration release -o c:\HardyWebsite\publish 
+
+C:\Windows\System32\inetsrv\appcmd.exe SET SITE "Default Web Site" /physicalPath:c:"c:\HardyWebsite\publish"
 
 C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {
              Import-Module WebAdministration
