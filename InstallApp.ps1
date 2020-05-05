0ed135237337eb85569d42476c7a40b8869ce38a
Hardy Thrower <hardyathrower@gmail.com>
2020-05-05 00:00:26 -0400

    fixed install yay

--- InstallApp.ps1
+++ InstallApp.ps1
@@ -25,10 +25,8 @@ Write-Warning "Original arguments (if any): $args"
 # Your 64-bit script code follows here...
 # TODO find out better way to call dotnet instead of using a hard coded location.
 # Unfortuantely there are multiple versions of  dotnet installed and the deployment always chooses the wrong one :(
-$current = pwd
-echo $current
-C:\Users\Administrator\AppData\Local\Microsoft\dotnet\dotnet.exe restore
-C:\Users\Administrator\AppData\Local\Microsoft\dotnet\dotnet.exe publish --configuration release -o c:\HardyWebsite\publish 
+C:\Users\Administrator\AppData\Local\Microsoft\dotnet\dotnet.exe restore C:/HardyWebsite
+C:\Users\Administrator\AppData\Local\Microsoft\dotnet\dotnet.exe publish C:/HardyWebsite --configuration release -o c:\HardyWebsite\publish 
 
 C:\Windows\System32\inetsrv\appcmd.exe stop site "HardyWebsite"
 C:\Windows\System32\inetsrv\appcmd.exe start site "HardyWebsite" 
