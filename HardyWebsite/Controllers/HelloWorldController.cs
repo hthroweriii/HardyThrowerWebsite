a2c7a0d8511c068574fbffba82cf07db14c82cfa
Hardy Thrower <hardyathrower@gmail.com>
2020-05-10 14:52:22 -0400

    Add New HellowWorld Cotroller with caching

---
+++ HardyWebsite/Controllers/HelloWorldController.cs
@@ -0,0 +1,53 @@
+﻿using HardyWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private IDistributedCache distributedCache;
+
+        // GET: /HelloWorld/
+        public HelloWorldController(IDistributedCache distributedCache)
+        {
+            this.distributedCache = distributedCache;
+        }
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string SetPizza(PizzaModel pizzaModel)
+        {
+            distributedCache.Set(pizzaModel.type,BitConverter.GetBytes(pizzaModel.amount));
+            return pizzaModel.type;
+        }
+
+        public string GetPizza(string type)
+        {
+            byte[] amt;
+            if (type == null)
+                throw new ArgumentNullException();
+            else
+            {
+                amt = distributedCache.Get(type);
+                if (amt == null)
+                    return "Not Found";
+            }
+            return HtmlEncoder.Default.Encode($"Hello {type}, NumTimes is: {BitConverter.ToInt16(amt)}");
+        }   
+
+        public string Clear(string type)
+        {
+            if(type != null)
+                distributedCache.Remove(type);
+            return "Cleared Cache";
+        }
+    }
+}
---
+++ HardyWebsite/Models/PizzaModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HardyWebsite.Models
+{
+    [BindProperties(SupportsGet = true)]
+    public class PizzaModel
+    {
+        public string type { get; set; }
+        public int amount { get; set; }
+        public PizzaModel()
+        {
+        }
+    }
+}
